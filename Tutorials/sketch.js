ce51fa8506f621d4aef92534459519d80ade8647
dyego-eu <dyego.eu@gmail.com>
2020-05-04 02:41:08 -0300

    finished first tutorial

--- Tutorials/sketch.js
+++ Tutorials/sketch.js
@@ -1,24 +1,17 @@
-var i;
-var j;
-var k;
+var ballSize;
 
 function setup(){
     createCanvas(650,480);
-    i = 0;
-    j = 0;
-    k = 0;
+    ballSize=100;
 }
 
 function draw() {
-    i+=11;
-    j+=12;
-    k+=13
-    strokeWeight(20);
-    stroke(10,10,100);
-    fill(
-        i%512 < 255 ? i%512 : 512 - i%512, 
-        j%512 < 255 ? j%512 : 512 - j%512,
-        k%512 < 255 ? k%512 : 512 - k%512
-    );
-    rect(0,0,100,100)
+    background(200);
+    stroke(0);
+    strokeWeight(3);
+    ellipse(mouseX, mouseY, ballSize, ballSize);
+}
+
+function mouseWheel(event){
+    ballSize += event.delta;
 }
